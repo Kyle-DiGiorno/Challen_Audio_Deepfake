05bc4141a8d2f369b141e65a044280ed8ea569a2
Josh <ylim31@illinois.edu>
2019-11-29 16:40:29 -0600

    did shit

--- app/src/main/java/com/example/challenaudiodeepfake/MainActivity.java
+++ app/src/main/java/com/example/challenaudiodeepfake/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.challenaudiodeepfake;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private Button submitButton;
+    private EditText inputText;
+    private String inputTextAsString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        submitButton = findViewById(R.id.submitButton);
+        inputText = findViewById(R.id.inputText);
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                inputTextAsString = inputText.getText().toString();
+            }
+        });
     }
 }
