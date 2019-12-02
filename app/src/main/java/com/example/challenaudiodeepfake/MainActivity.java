2b4dffb23beffa2ad65ac46b98dcd0a65e507790
Josh <ylim31@illinois.edu>
2019-12-02 16:55:35 -0600

    did shit

--- app/src/main/java/com/example/challenaudiodeepfake/MainActivity.java
+++ app/src/main/java/com/example/challenaudiodeepfake/MainActivity.java
@@ -3,17 +3,23 @@ package com.example.challenaudiodeepfake;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.location.GnssNavigationMessage;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
     private Button submitButton;
     private EditText inputText;
     private String inputTextAsString;
     private LinearLayout popupLayout;
+    private SeekBar seekBar;
+    private Button pauseButton;
+    private Button startButton;
+    private MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +29,9 @@ public class MainActivity extends AppCompatActivity {
         submitButton = findViewById(R.id.submitButton);
         inputText = findViewById(R.id.inputText);
         popupLayout = findViewById(R.id.popupLayout);
+        seekBar = findViewById(R.id.seekBar);
+        pauseButton = findViewById(R.id.pauseButton);
+        startButton = findViewById(R.id.startButton);
 
         popupLayout.setVisibility(View.GONE);
 
@@ -31,9 +40,10 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 inputTextAsString = inputText.getText().toString();
                 popupLayout.setVisibility(View.VISIBLE);
-
             }
         });
+
+
     }
 
 }
