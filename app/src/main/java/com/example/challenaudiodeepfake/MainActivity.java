e0af0910f8162ebf072b684001b8ded6f28c573c
Josh <ylim31@illinois.edu>
2019-12-03 17:04:37 -0600

    added mediaplayer/play button

--- app/src/main/java/com/example/challenaudiodeepfake/MainActivity.java
+++ app/src/main/java/com/example/challenaudiodeepfake/MainActivity.java
@@ -6,6 +6,8 @@ import android.graphics.Point;
 import android.location.GnssNavigationMessage;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.Display;
 import android.view.View;
 import android.widget.Button;
@@ -19,11 +21,13 @@ public class MainActivity extends AppCompatActivity {
     private String inputTextAsString;
     private LinearLayout popupLayout;
     private SeekBar seekBar;
-    private Button pauseButton;
+    //private Button pauseButton;
     private Button startButton;
     private MediaPlayer mediaPlayer;
+    private int timeLength;
     private int width;
     private int height;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
         inputText = findViewById(R.id.inputText);
         popupLayout = findViewById(R.id.popupLayout);
         seekBar = findViewById(R.id.seekBar);
-        pauseButton = findViewById(R.id.pauseButton);
+        //pauseButton = findViewById(R.id.pauseButton);
         startButton = findViewById(R.id.startButton);
 
         popupLayout.setVisibility(View.GONE);
@@ -51,7 +55,69 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.hide);
+        mediaPlayer.setLooping(true);
+        mediaPlayer.seekTo(0);
+        mediaPlayer.setVolume(1, 1);
+        timeLength = mediaPlayer.getDuration();
+
+        seekBar.setMax(timeLength);
+        seekBar.setOnSeekBarChangeListener(
+                new SeekBar.OnSeekBarChangeListener() {
+                    @Override
+                    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                        if (fromUser == true) {
+                            mediaPlayer.seekTo(progress);
+                            seekBar.setProgress(progress);
+                        }
+                    }
+
+                    @Override
+                    public void onStartTrackingTouch(SeekBar seekBar) {
+
+                    }
+
+                    @Override
+                    public void onStopTrackingTouch(SeekBar seekBar) {
+
+                    }
+                }
+        );
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (mediaPlayer != null) {
+                    try {
+                        Message msg = new Message();
+                        msg.what = mediaPlayer.getCurrentPosition();
+                        handler.sendMessage(msg);
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e){ }
+                }
+            }
+        }).start();
     }
+
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            int currentPosition = msg.what;
+            seekBar.setProgress(currentPosition);
+        }
+    };
+
+    public void playButton(View view) {
+        if (!mediaPlayer.isPlaying()) {
+            mediaPlayer.start();
+            startButton.setBackgroundResource(R.drawable.stop);
+        } else {
+            mediaPlayer.pause();
+            startButton.setBackgroundResource(R.drawable.play);
+        }
+    }
+
     public int getWidth() {
         return width;
     }
