b3e5756117ae6746bc639c3df0efccecbf974f2b
Josh <ylim31@illinois.edu>
2019-12-10 16:27:48 -0600

    Fixed intent! Now can upload files and play it using the media player

--- app/src/main/java/com/example/challenaudiodeepfake/MainActivity.java
+++ app/src/main/java/com/example/challenaudiodeepfake/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.challenaudiodeepfake;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
+import android.app.DownloadManager;
+import android.content.Context;
+import android.content.Intent;
 import android.graphics.Point;
 import android.location.GnssNavigationMessage;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -17,11 +22,16 @@ import android.view.animation.AnimationSet;
 import android.view.animation.AnimationUtils;
 import android.view.animation.LinearInterpolator;
 import android.view.animation.TranslateAnimation;
+import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.SeekBar;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 public class MainActivity extends AppCompatActivity {
     private Button submitButton;
@@ -29,12 +39,21 @@ public class MainActivity extends AppCompatActivity {
     private SeekBar seekBar;
     //private Button pauseButton;
     private Button startButton;
+
     private MediaPlayer mediaPlayer;
     private int timeLength;
-    private int width;
-    private int height;
+
     private ImageView challenmouthflap;
+    private Button uploadButton;
     private android.view.animation.Animation animation;
+
+    private Intent myFileIntent;
+
+    private Button downloadButton;
+    private DownloadManager downloadManager;
+    private Uri file;
+
+    private String filePath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,8 +62,7 @@ public class MainActivity extends AppCompatActivity {
         Display display = getWindowManager().getDefaultDisplay();
         Point size = new Point();
         display.getSize(size);
-        int width = size.x;
-        int height = size.y;
+
         submitButton = findViewById(R.id.submitButton);
 
         popupLayout = findViewById(R.id.popupLayout);
@@ -52,64 +70,39 @@ public class MainActivity extends AppCompatActivity {
         //pauseButton = findViewById(R.id.pauseButton);
         startButton = findViewById(R.id.startButton);
         challenmouthflap = findViewById(R.id.challenmouthflap);
+        uploadButton = findViewById(R.id.uploadButton);
+        downloadButton = findViewById(R.id.downloadButton);
+
+
+
+        //setUploadButton();
+        /*
+        downloadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                downloadManager = (DownloadManager)getSystemService(Context.DOWNLOAD_SERVICE);
+                //Uri uri = Uri.parse();
+                DownloadManager.Request request = new DownloadManager.Request(uri);
+                request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+                Long reference = downloadManager.enqueue(request);
+
+            }
+        });
+
+         */
+
 
 
 
         popupLayout.setVisibility(View.GONE);
 
-        submitButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                popupLayout.setVisibility(View.VISIBLE);
-            }
-        });
+        submittingButton();
+        setUploadButton();
 
 
 
-        mediaPlayer = MediaPlayer.create(this, R.raw.hide);
-        mediaPlayer.setLooping(true);
-        mediaPlayer.seekTo(0);
-        mediaPlayer.setVolume(5, 5);
-        timeLength = mediaPlayer.getDuration();
-
-        seekBar.setMax(timeLength);
-        seekBar.setOnSeekBarChangeListener(
-                new SeekBar.OnSeekBarChangeListener() {
-                    @Override
-                    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                        if (fromUser == true) {
-                            mediaPlayer.seekTo(progress);
-                            seekBar.setProgress(progress);
 
 
-                        }
-                    }
-
-                    @Override
-                    public void onStartTrackingTouch(SeekBar seekBar) {
-
-                    }
-
-                    @Override
-                    public void onStopTrackingTouch(SeekBar seekBar) {
-
-                    }
-                }
-        );
-
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                while (mediaPlayer != null) {
-                    try {
-                        Message msg = new Message();
-                        msg.what = mediaPlayer.getCurrentPosition();
-                        handler.sendMessage(msg);
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e){ }
-                }
-            }
-        }).start();
     }
 
     private Handler handler = new Handler() {
@@ -190,7 +183,7 @@ public class MainActivity extends AppCompatActivity {
             startButton.setBackgroundResource(R.drawable.stop);
 
 
-
+// Colin was here
         } else {
             mediaPlayer.pause();
             animation.cancel();
@@ -198,12 +191,112 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public int getWidth() {
-        return width;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        switch (requestCode) {
+            case 10:
+                if (resultCode == RESULT_OK) {
+                    if (data != null && data.getData() != null) {
+                        //filePath = data.getData().getPath();
+                        file = data.getData();
+                    }
+
+                }
+                break;
+        }
     }
-    public int getHeight() {
-        return height;
+
+    public void setUploadButton(){
+        uploadButton.setOnClickListener(new View.OnClickListener()  {
+            @Override
+            public void onClick(View v)  {
+                /*
+                try {
+                    FileInputStream hideAndSeek = new FileInputStream("hide.mp3");
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+
+                 */
+
+
+
+                myFileIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                myFileIntent.setType("*/*");
+                filePath = myFileIntent.getStringExtra("GetPath");
+                startActivityForResult(myFileIntent, 10);
+
+
+
+
+
+
+            }
+        });
+
+
     }
+
+    public void submittingButton() {
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                popupLayout.setVisibility(View.VISIBLE);
+
+                    mediaPlayer = MediaPlayer.create(MainActivity.this, file);
+                    mediaPlayer.setLooping(true);
+                    mediaPlayer.seekTo(0);
+                    mediaPlayer.setVolume(5, 5);
+                    timeLength = mediaPlayer.getDuration();
+
+                seekBar.setMax(timeLength);
+                seekBar.setOnSeekBarChangeListener(
+                        new SeekBar.OnSeekBarChangeListener() {
+                            @Override
+                            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                                if (fromUser == true) {
+                                    mediaPlayer.seekTo(progress);
+                                    seekBar.setProgress(progress);
+
+
+                                }
+                            }
+
+                            @Override
+                            public void onStartTrackingTouch(SeekBar seekBar) {
+
+                            }
+
+                            @Override
+                            public void onStopTrackingTouch(SeekBar seekBar) {
+
+                            }
+                        }
+                );
+
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        while (mediaPlayer != null) {
+                            try {
+                                Message msg = new Message();
+                                msg.what = mediaPlayer.getCurrentPosition();
+                                handler.sendMessage(msg);
+                                Thread.sleep(1000);
+                            } catch (InterruptedException e){ }
+                        }
+                    }
+                }).start();
+
+            }
+        });
+
+    }
+
+
+
+
 
 
 
